fef306b211a1f3d09e70aa8983add11745e6ced7
Takumi Sato <sato@sirok.co.jp>
2021-12-12 10:59:00 +0900

    Add eslint / prettier

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
-const { app, BrowserWindow, Menu, Tray, nativeImage, shell, Notification, ipcMain, dialog, nativeTheme} = require('electron')
+const {
+  app,
+  BrowserWindow,
+  Menu,
+  Tray,
+  nativeImage,
+  shell,
+  Notification,
+  ipcMain,
+  dialog,
+  nativeTheme,
+} = require('electron')
 const PdApi = require('./src/pd_api')
-const Store = require('electron-store');
+const Store = require('electron-store')
 const dayjs = require('dayjs')
-const store = new Store();
+const store = new Store()
 const interval = 2
 
 function createSettingWindow(config) {
@@ -16,117 +27,148 @@ function createSettingWindow(config) {
     skipTaskbar: true,
     webPreferences: {
       contextIsolation: true,
-      preload: __dirname + '/src/browser/preload.js' 
-    }
-  });
+      preload: __dirname + '/src/browser/preload.js',
+    },
+  })
   mainWindow.loadFile(__dirname + '/src/browser/index.html')
 
-  mainWindow.webContents.on('did-finish-load', ()=>{
-    mainWindow.webContents.send('load_config', config);
-  });
+  mainWindow.webContents.on('did-finish-load', () => {
+    mainWindow.webContents.send('load_config', config)
+  })
 
   mainWindow.on('close', (event) => {
-    app.dock.hide();
-  });
+    app.dock.hide()
+  })
 }
 
-function createWindow () {
-  const apiKey = store.get('apiKey', '');
-  const userId = store.get('userId', '');
+function createWindow() {
+  const apiKey = store.get('apiKey', '')
+  const userId = store.get('userId', '')
 
   if (apiKey === '' || userId === '') {
-    createSettingWindow({apiKey, userId});
-    return;
+    createSettingWindow({ apiKey, userId })
+    return
   }
 
   app.dock.hide()
   const tray = new Tray(__dirname + '/assets/images/icon.png')
   // 通知済みインシデント
-  const notified_incident_ids = [];
+  const notified_incident_ids = []
   // 設定メニュー
-  const configMenu= {
+  const configMenu = {
     label: 'Setting',
     click: () => {
-      createSettingWindow({apiKey, userId});
-      app.dock.show();
-    }
+      createSettingWindow({ apiKey, userId })
+      app.dock.show()
+    },
   }
 
-  const contextMenu = Menu.buildFromTemplate([{ label: 'Loading...', type: 'normal' }, configMenu]);
+  const contextMenu = Menu.buildFromTemplate([
+    { label: 'Loading...', type: 'normal' },
+    configMenu,
+  ])
   tray.setContextMenu(contextMenu)
 
-  const pdApi = new PdApi(apiKey);
+  const pdApi = new PdApi(apiKey)
   // ３秒おきに処理
   setInterval(() => {
-    pdApi.fetchTriggeredIncidents(userId, dayjs().subtract(600, 'minute').toISOString())
+    pdApi
+      .fetchTriggeredIncidents(
+        userId,
+        dayjs().subtract(600, 'minute').toISOString()
+      )
       .then((incidents) => {
-        console.log(incidents);
-        if (incidents.find(incident => incident['status'] === 'triggered')) {
-          tray.setImage(generateNativeImageIcon('red'));
+        console.log(incidents)
+        if (incidents.find((incident) => incident['status'] === 'triggered')) {
+          tray.setImage(generateNativeImageIcon('red'))
         } else if (incidents.length > 0) {
-          tray.setImage(generateNativeImageIcon());
+          tray.setImage(generateNativeImageIcon())
         } else {
-          tray.setImage(generateNativeImageIcon(nativeTheme.shouldUseDarkColors ? 'white' : 'gray'));
+          tray.setImage(
+            generateNativeImageIcon(
+              nativeTheme.shouldUseDarkColors ? 'white' : 'gray'
+            )
+          )
         }
 
-        const contextMenuTemplate = [];
+        const contextMenuTemplate = []
 
         // インシデントを1個ずつ処理する
-        incidents.forEach(incident => {
+        incidents.forEach((incident) => {
           // メニューに追加
           contextMenuTemplate.push({
             label: `[${incident['service']['summary']}]${incident['title']}`,
-            icon: incident['status'] === 'triggered' ? generateNativeImageIcon('red') : generateNativeImageIcon('yellow'),
+            icon:
+              incident['status'] === 'triggered'
+                ? generateNativeImageIcon('red')
+                : generateNativeImageIcon('yellow'),
             click: () => {
-              shell.openExternal(incident['html_url']);
-            }
+              shell.openExternal(incident['html_url'])
+            },
           })
           // 通知済み、もしくはAck済みはスキップ
-          if (notified_incident_ids.includes(incident['incident_number']) || incident['status'] === 'acknowledged' ) {
+          if (
+            notified_incident_ids.includes(incident['incident_number']) ||
+            incident['status'] === 'acknowledged'
+          ) {
             return
           }
 
           // 通知
-          const notification = new Notification({title: incident['title'], body: incident['service']['summary']});
-          notification.show();
-          notification.on('click', (event, arg)=>{
-            shell.openExternal(incident['html_url']);
-          });
+          const notification = new Notification({
+            title: incident['title'],
+            body: incident['service']['summary'],
+          })
+          notification.show()
+          notification.on('click', (event, arg) => {
+            shell.openExternal(incident['html_url'])
+          })
           notified_incident_ids.push(incident['incident_number'])
-        });
+        })
 
         // メニューを更新
-        contextMenuTemplate.push(configMenu);
-        tray.setContextMenu(Menu.buildFromTemplate(contextMenuTemplate))        
-      }).catch(() => {})
+        contextMenuTemplate.push(configMenu)
+        tray.setContextMenu(Menu.buildFromTemplate(contextMenuTemplate))
+      })
+      .catch(() => {})
   }, interval * 1000)
 }
 
 // コンフィグを保存
-ipcMain.on("save_config", (event, {apiKey, userId}) => {
-  store.set('apiKey', apiKey);
-  store.set('userId', userId);
+ipcMain.on('save_config', (event, { apiKey, userId }) => {
+  store.set('apiKey', apiKey)
+  store.set('userId', userId)
 
   dialog.showMessageBox({
     type: 'info',
     title: 'Success',
     message: 'Config Saved',
-    detail: 'Please relaunch application.'
-  });
+    detail: 'Please relaunch application.',
+  })
 
-  process.exit(0);
-});
+  process.exit(0)
+})
 
 function generateNativeImageIcon(color = null) {
-  let nativeImageFile = nativeImage.createFromPath(__dirname + '/assets/images/icon.png');
+  let nativeImageFile = nativeImage.createFromPath(
+    __dirname + '/assets/images/icon.png'
+  )
   if (color === 'gray') {
-    nativeImageFile = nativeImage.createFromPath(__dirname + '/assets/images/icon_gray.png');
+    nativeImageFile = nativeImage.createFromPath(
+      __dirname + '/assets/images/icon_gray.png'
+    )
   } else if (color === 'white') {
-    nativeImageFile = nativeImage.createFromPath(__dirname + '/assets/images/icon_white.png');
+    nativeImageFile = nativeImage.createFromPath(
+      __dirname + '/assets/images/icon_white.png'
+    )
   } else if (color === 'yellow') {
-    nativeImageFile = nativeImage.createFromPath(__dirname + '/assets/images/icon_yellow.png');
+    nativeImageFile = nativeImage.createFromPath(
+      __dirname + '/assets/images/icon_yellow.png'
+    )
   } else if (color === 'red') {
-    nativeImageFile = nativeImage.createFromPath(__dirname + '/assets/images/icon_red.png');
+    nativeImageFile = nativeImage.createFromPath(
+      __dirname + '/assets/images/icon_red.png'
+    )
   }
   return nativeImageFile
 }
